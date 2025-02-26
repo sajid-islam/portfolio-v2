671e6d5d5d4d07936f09c3928b6573cf614632f8
sajid islam <sajidisalm941@gmail.com>
2025-02-26 13:25:00 +0600

    Impelement project details dialog

--- src/components/portfolioShowcaseComponents/FeaturedProjects.jsx
+++ src/components/portfolioShowcaseComponents/FeaturedProjects.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { Card, CardContent } from "../ui/card";
 import { cn } from "@/lib/utils";
-import ViewDetailsDialog from "../sidebarComponents/ViewDetailsDialog";
+import ViewDetailsDialog from "./ViewDetailsDialog";
 
 const projects = [
     {
@@ -24,6 +24,16 @@ const projects = [
             "Progress Tracking: Monitor student performance and engagement.",
             "Google Meet Integration: Seamless video conferencing for live classes.",
         ],
+        screenshots: ["/projectsImg/virtual-classroom.png"],
+        link: "https://class-net.vercel.app",
+        repoLinks: [
+            {
+                clientSide:
+                    "https://github.com/sajid-islam/Virtual-Classroom-Client",
+                serverSide:
+                    "https://github.com/sajid-islam/Virtual-Classroom-Server",
+            },
+        ],
     },
     {
         name: "Employee Management",
@@ -43,6 +53,16 @@ const projects = [
             "Performance Evaluation: Track and review employee performance.",
             "Role-Based Access Control: Secure data access for admins and employees.",
         ],
+        screenshots: ["/projectsImg/employee-management.png"],
+        link: "https://a12-employee-management-6ca56.web.app/",
+        repoLinks: [
+            {
+                clientSide:
+                    "https://github.com/sajid-islam/Employee-Management-Client",
+                serverSide:
+                    "https://github.com/sajid-islam/Employee-Management-Server",
+            },
+        ],
     },
     {
         name: "Agency and Blogging Platform",
@@ -60,6 +80,13 @@ const projects = [
             "Agency Portfolio: Showcase services with dynamic agency pages.",
             "Secure Authentication: User login and access management using Clerk.",
             "Rich Blogging System: Create, edit, and manage blog posts effortlessly.",
+        ],
+        screenshots: ["/projectsImg/agency-blogging.png"],
+        repoLinks: [
+            {
+                clientSide:
+                    "https://github.com/sajid-islam/Agency-Blogging-Platform",
+            },
         ],
     },
     {
@@ -81,6 +108,13 @@ const projects = [
             "LocalStorage Support: Persist tasks without database dependency.",
             "Priority Sorting: Organize tasks based on urgency.",
         ],
+        screenshots: ["/projectsImg/task-management.png"],
+        link: "https://dohub.vercel.app/",
+        repoLinks: [
+            {
+                clientSide: "https://github.com/sajid-islam/Task-Management",
+            },
+        ],
     },
     {
         name: "Blogging Platform",
@@ -99,6 +133,14 @@ const projects = [
             "User-Friendly Blog Creation: Easily create and publish tech-related blogs.",
             "Interactive Commenting System: Engage with other users through comments on blog posts.",
             "Wishlist Management: Save favorite blogs to a wishlist for quick access.",
+        ],
+        screenshots: ["/projectsImg/blogging-platform.png"],
+        link: "https://a11-devnook.web.app/",
+        repoLinks: [
+            {
+                clientSide: "https://github.com/sajid-islam/Dev-Nook-Client",
+                serverSide: "https://github.com/sajid-islam/Dev-Nook-Server",
+            },
         ],
     },
 ];
@@ -121,9 +163,11 @@ const FeaturedProjects = () => {
                         <Card key={i}>
                             <CardContent className="pt-6 h-full">
                                 <div className="flex flex-col h-full">
-                                    <h4 className="font-semibold text-primary hover:underline">
-                                        {project.name}
-                                    </h4>
+                                    <Link href={project.link ? project.link : "#"} target="_blank">
+                                        <h4 className="font-semibold text-primary hover:underline">
+                                            {project.name}
+                                        </h4>
+                                    </Link>
                                     <p className="text-sm text-muted-foreground mt-1 mb-4">
                                         {project.description}
                                     </p>
@@ -139,7 +183,14 @@ const FeaturedProjects = () => {
                                                 {project.tech}
                                             </span>
                                         </div>
-                                        <ViewDetailsDialog name={project.name}/>
+                                        <ViewDetailsDialog
+                                            name={project.name}
+                                            screenshots={project.screenshots}
+                                            technologies={project.technologies}
+                                            features={project.features}
+                                            link={project.link}
+                                            repoLinks={project.repoLinks}
+                                        />
                                     </div>
                                 </div>
                             </CardContent>
---
+++ src/components/portfolioShowcaseComponents/ViewDetailsDialog.jsx
@@ -0,0 +1,126 @@
+import Image from "next/image";
+import { FaGithub } from "react-icons/fa6";
+
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "../ui/dialog";
+import { BiDetail } from "react-icons/bi";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { ExternalLink } from "lucide-react";
+
+const ViewDetailsDialog = ({
+    name,
+    screenshots,
+    technologies,
+    features,
+    link,
+    repoLinks,
+}) => {
+    return (
+        <div>
+            <Dialog>
+                <DialogTrigger>
+                    <div className="flex items-center gap-2 text-sm text-primary hover:underline cursor-pointer">
+                        View Details
+                        <BiDetail className="inline-block size-4" />
+                    </div>{" "}
+                </DialogTrigger>
+                <DialogContent className="w-full max-w-3xl h-[90vh] overflow-auto custom-scrollbar">
+                    <DialogHeader>
+                        <DialogTitle>Project Details</DialogTitle>
+                    </DialogHeader>
+                    <div className="mx-auto max-h-[72vh] overflow-auto space-y-4 custom-scrollbar">
+                        <div>
+                            {screenshots.map((screenshot, i) => (
+                                <div key={i} className="border-2 rounded-lg">
+                                    <Image
+                                        src={screenshot}
+                                        className="rounded-lg"
+                                        alt={"screenshot of" + name}
+                                        width={700}
+                                        height={200}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                        <div>
+                            <h5 className="font-semibold">Technologies:</h5>
+                            <div className="flex flex-wrap gap-2 mt-2">
+                                {technologies.map((t, i) => (
+                                    <p
+                                        key={i}
+                                        className="text-sm bg-[#f5f5f5] dark:bg-[#262626] rounded-full px-[10px] py-1"
+                                    >
+                                        {t}
+                                    </p>
+                                ))}
+                            </div>
+                        </div>
+                        <div>
+                            <h5 className="font-semibold">Features:</h5>
+                            <ul className="mt-2 list-disc">
+                                {features.map((f, i) => (
+                                    <li key={i} className="flex gap-1">
+                                        •<strong>{f.split(":")[0]}:</strong>
+                                        <p>{f.split(":")[1]}</p>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                    <DialogFooter className="">
+                        {repoLinks.map((repoLink, i) => (
+                            <div
+                                key={i}
+                                className="flex flex-col md:flex-row gap-2"
+                            >
+                                <Link
+                                    href={repoLink.clientSide}
+                                    target="_blank"
+                                >
+                                    <Button className="w-full md:w-auto">
+                                        <FaGithub />
+                                        {!repoLink.serverSide
+                                            ? "Project Repo"
+                                            : "Frontend Repo"}
+                                    </Button>
+                                </Link>
+                                {repoLink.serverSide && (
+                                    <Link
+                                        href={repoLink.serverSide}
+                                        target="_blank"
+                                    >
+                                        <Button className="w-full md:w-auto">
+                                            {" "}
+                                            <FaGithub />
+                                            Backend Repo
+                                        </Button>
+                                    </Link>
+                                )}
+                            </div>
+                        ))}
+                        {link && (
+                            <Link
+                                href={link}
+                                target="_blank"
+                                className="mb-2 lg:mb-0"
+                            >
+                                <Button className="w-full">
+                                    View Project <ExternalLink />{" "}
+                                </Button>
+                            </Link>
+                        )}
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </div>
+    );
+};
+
+export default ViewDetailsDialog;
