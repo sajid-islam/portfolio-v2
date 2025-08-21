d0fd50679e2a8c7697eebf153f78597a4bfb1ddf
sajid-islam <sajidislam941@gmail.com>
2025-08-21 20:15:28 +0600

    skill: add next.js and redux in skill section

--- src/components/sidebarComponents/SkillsSection.jsx
+++ src/components/sidebarComponents/SkillsSection.jsx
@@ -2,7 +2,7 @@ import { FaHtml5 } from "react-icons/fa";
 import { FaCss3, FaGit, FaGithub, FaReact } from "react-icons/fa6";
 import { IoLogoJavascript } from "react-icons/io5";
 import { FaNodeJs } from "react-icons/fa";
-import { SiExpress } from "react-icons/si";
+import { SiExpress, SiNextdotjs, SiRedux } from "react-icons/si";
 import { SiMongodb } from "react-icons/si";
 import { SiTailwindcss } from "react-icons/si";
 import { SiFirebase } from "react-icons/si";
@@ -17,7 +17,11 @@ const skills = [
         icon: <FaCss3 size={15} />,
     },
     {
-        name: "TailwindCSS",
+        name: "React",
+        icon: <FaReact size={15} />,
+    },
+    {
+        name: "Tailwind",
         icon: <SiTailwindcss size={15} />,
     },
     {
@@ -25,15 +29,15 @@ const skills = [
         icon: <IoLogoJavascript size={15} />,
     },
     {
-        name: "NodeJS",
+        name: "Node.js",
         icon: <FaNodeJs size={15} />,
     },
     {
-        name: "React",
-        icon: <FaReact size={15} />,
+        name: "Next.js",
+        icon: <SiNextdotjs size={15} />,
     },
     {
-        name: "ExpreesJS",
+        name: "Exprees.js",
         icon: <SiExpress size={15} />,
     },
     {
@@ -48,16 +52,23 @@ const skills = [
         name: "FirebaseAuth",
         icon: <SiFirebase size={15} />,
     },
+    {
+        name: "Redux",
+        icon: <SiRedux size={15} />,
+    },
 ];
 const SkillsSection = () => {
     return (
         <section className="border p-6 rounded-lg mt-5">
             <h2 className="text-2xl font-bold">Skills</h2>
-            <div className="flex flex-wrap gap-2 mt-6">
+            <div className="flex flex-wrap gap-1.5 mt-6">
                 {skills.map((skill, idx) => (
-                    <p key={idx} className="text-[12px] font-semibold flex items-center gap-1 bg-[#f5f5f5] dark:bg-[#262626] rounded-full px-[10px] py-1">
-                      {skill.icon}
-                      <span>{skill.name}</span>
+                    <p
+                        key={idx}
+                        className="text-[12px] font-semibold flex items-center gap-1 bg-[#f5f5f5] dark:bg-[#262626] rounded-full px-[10px] py-1"
+                    >
+                        {skill.icon}
+                        <span>{skill.name}</span>
                     </p>
                 ))}
             </div>
