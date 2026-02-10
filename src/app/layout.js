d458fe3a73778683a53258359fb127f9040c4de8
sajid-islam <sajidislam941@gmail.com>
2026-02-10 11:17:28 +0600

    fix: name spelling and metadata description

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,29 +16,29 @@ const inter = Inter({
 export const metadata = {
     title: "</Sajid> - Portfolio",
     description:
-        "Welcome to my portfolio! I'm Md Sajiul Islam, a passionate developer specializing in building modern web applications.",
+        "Browse Md Sajidul Islam's  portfolio. Expert in Javascript/typescript and React/Next.js, and scalable web apps. See my recent work and contact me for your next project",
     keywords:
-        "Md Sajiul Islam, portfolio, developer, web development, next.js, react, frontend, backend , Sajid Islam, Sajid, Md Sajid, Md Sajidul, Web Developer, Web Developer in Bangladesh, Web Developer in khulna, React Developer, Next.js Developer, Sajiul Islam Web Developer, Md Sajiul Islam Portfolio, Sajid Islam React Developer",
-    authors: [{ name: "Md Sajiul Islam", url: "https://sajid-islam.vercel.app" }],
-    creator: "Md Sajiul Islam",
-    publisher: "Md Sajiul Islam",
+        "Md Sajidul Islam, portfolio, developer, web development, next.js, react, frontend, backend , Sajid Islam, Sajid, Md Sajid, Md Sajidul, Web Developer, Web Developer in Bangladesh, Web Developer in khulna, React Developer, Next.js Developer, Sajidul Islam Web Developer, Md Sajidul Islam Portfolio, Sajid Islam React Developer",
+    authors: [{ name: "Md Sajidul Islam", url: "https://sajid-islam.vercel.app" }],
+    creator: "Md Sajidul Islam",
+    publisher: "Md Sajidul Islam",
     icons: {
         icon: "/me.png",
         shortcut: "/me.png",
         apple: "/me.png",
     },
     openGraph: {
-        title: "Md Sajiul Islam - Portfolio",
+        title: "Md Sajidul Islam - Portfolio",
         description:
-            "Welcome to my portfolio! I'm Md Sajiul Islam, a passionate developer specializing in building modern web applications.",
+            "Welcome to my portfolio! I'm Md Sajidul Islam, a passionate developer specializing in building modern web applications.",
         url: "https://sajid-islam.vercel.app",
-        siteName: "Md Sajiul Islam",
+        siteName: "Md Sajidul Islam",
         images: [
             {
                 url: "/me.png",
                 width: 1200,
                 height: 630,
-                alt: "Md Sajiul Islam's Portfolio",
+                alt: "Md Sajidul Islam's Portfolio",
             },
         ],
         locale: "en_US",
